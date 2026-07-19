b4933a4cd95a32b4284e75b8e2a96bd54fb08f27
Naveennaidu548 <upattabhiram@gmail.com>
2026-07-19 21:54:33 +0530

    completed

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB(){
     try{
-        await mongoose.connect("mongodb+srv://saibalaji:DDWZYEIX7oWlzRoP@cluster0.245dy24.mongodb.net/blogsDB?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("MongoDB connected")
     }catch(err){
         console.log(err)
